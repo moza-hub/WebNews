481af2b1cb8777a997568a7ddadd157d67a12564
Hao Tran Quang <quanghao332000@gmail.com>
2021-07-03 11:12:46 +0700

    Add project files.

---
+++ WebNews/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebNews.Startup))]
+namespace WebNews
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
